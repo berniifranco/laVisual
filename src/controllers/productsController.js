72b610865a03779a0fa1d479f6866fbf02193ef9
Bernardo Franco <berniifranco@gmail.com>
2022-10-12 04:27:17 -0300

    CRUD productos empezado

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,6 +1,71 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const ferreteriaFilePath = path.join(__dirname, '../data/ferreteriaDataBase.json');
+const ferreteria = JSON.parse(fs.readFileSync(ferreteriaFilePath, 'utf-8'));
+const { validationResult } = require('express-validator');
+
+
 const controller = {
-    index: (req, res) => {
-        res.send('respond with a resource')
+    ferreteria: (req, res) => {
+        let idUser = null;
+
+        if (req.session.usuarioLogueado != undefined) {
+            idUser = req.session.usuarioLogueado.id;
+        }
+
+        const ferreteria = JSON.parse(fs.readFileSync(ferreteriaFilePath, 'utf-8'));
+        res.render('products', { title: 'Ferretería', id: idUser, productos: ferreteria })
+    },
+    create: (req, res) => {
+        let idUser = null;
+
+        if (req.session.usuarioLogueado != undefined) {
+            idUser = req.session.usuarioLogueado.id;
+        };
+
+        res.render('product-create-form', { id: idUser })
+    },
+    store: (req, res) => {
+        let idNuevo = (ferreteria[ferreteria.length-1].id)+1;
+        let datos = req.body;
+        let errors = validationResult(req);
+
+        let idUser = null;
+
+        if (req.session.usuarioLogueado != undefined) {
+            idUser = req.session.usuarioLogueado.id;
+        };
+
+        if (errors.isEmpty()) {
+            let nuevoProducto = {
+                "id": idNuevo,
+                "name": datos.name,
+                "price": parseInt(datos.price),
+                "stock": parseInt(datos.stock),
+                "tipo": datos.tipo
+            };
+    
+            ferreteria.push(nuevoProducto);
+    
+            fs.writeFileSync(ferreteriaFilePath, JSON.stringify(ferreteria, null, 4), 'utf-8');
+    
+            res.redirect('/products/ferreteria');
+        } else {
+            res.render('product-create-form', { errors: errors.mapped(), id: idUser, oldData: datos })
+        }
+
+    },
+    delete: (req, res) => {
+        let idBus = req.params.id
+        
+        let nuevaLista = ferreteria.filter(function(e) {
+            return e.id != idBus
+        });
+
+        fs.writeFileSync(ferreteriaFilePath, JSON.stringify(nuevaLista, null, 4), 'utf-8');
+
+        res.redirect('/products/ferreteria');
     }
 };
 
--- src/routes/products.js
+++ src/routes/products.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const { body } = require('express-validator');
+
+let validacionesProductos = [
+    body('name').notEmpty().withMessage('Debes ingresar el nombre del producto'),
+    body('price').notEmpty().withMessage('Debes ingresar el precio del producto'),
+    body('stock').notEmpty().withMessage('Debes ingresar la cantidad de productos'),
+]
 
 const productsController = require('../controllers/productsController');
 
-router.get('/', productsController.index);
+router.get('/ferreteria', productsController.ferreteria);
+
+router.get('/create', productsController.create);
+router.post('/create', validacionesProductos, productsController.store);
+
+router.delete('/delete/:id', productsController.delete);
 
 module.exports = router;
